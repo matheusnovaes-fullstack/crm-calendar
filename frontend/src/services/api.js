2777e2bb479ebef163930cb15303764449289e06
matheusnovaes-fullstack <matheus.novaes@anagaming.com.br>
2026-03-03 12:20:50 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "http://localhost:3001/api" });
+const api = axios.create({ baseURL: "https://crm-calendar-backend.onrender.com/api" });
 
 export const getIssues = (projeto)  => api.get(`/issues/${projeto}`);
 export const getAnexos = (issueKey) => api.get(`/issues/${issueKey}/attachments`);
