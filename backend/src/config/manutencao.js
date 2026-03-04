3240356219c5c4a20d2630536569719f13c94c49
matheusnovaes-fullstack <matheus.novaes@anagaming.com.br>
2026-03-04 11:56:52 -0300

    Delete backend/src/config/manutencao.js

--- backend/src/config/manutencao.js
+++
@@ -1,9 +0,0 @@
-
-const MANUTENCAO = {
-  ativo:    false,
-  titulo:   "Sistema em Manutenção",
-  mensagem: "Estamos realizando melhorias para uma melhor experiência. Voltamos em breve!",
-  previsao: "",
-};
-
-export default MANUTENCAO;
