ba1f547b2c3651a8571bfcfa4fff0394760671fd
matheusnovaes-fullstack <matheus.novaes@anagaming.com.br>
2026-03-04 11:42:35 -0300

    Create manutencao.js

---
+++ backend/src/config/manutencao.js
@@ -0,0 +1,9 @@
+
+const MANUTENCAO = {
+  ativo:    false,
+  titulo:   "Sistema em Manutenção",
+  mensagem: "Estamos realizando melhorias para uma melhor experiência. Voltamos em breve!",
+  previsao: "",
+};
+
+export default MANUTENCAO;
