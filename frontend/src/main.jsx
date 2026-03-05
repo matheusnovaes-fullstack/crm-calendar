fa3b4a777b2d283fcef3b60a195a65a98c40a9db
matheusnovaes-fullstack <matheus.novaes@anagaming.com.br>
2026-03-05 10:06:37 -0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
     <App />
-  </StrictMode>,
-)
+  </GoogleOAuthProvider>
+);
