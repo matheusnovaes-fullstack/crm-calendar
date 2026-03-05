a9f0ae11eb24d4e9977f057da499f7cd8bc1cfd7
matheusnovaes-fullstack <matheus.novaes@anagaming.com.br>
2026-03-05 16:00:32 -0300

    Update manutencao.js

--- frontend/src/config/manutencao.js
+++ frontend/src/config/manutencao.js
@@ -2,7 +2,7 @@ const MANUTENCAO = {
   ativo:    true,
   titulo:   "Sistema em Manutenção",
   mensagem: "Estamos realizando melhorias no CRM Calendar. Voltamos em breve!",
-  previsao: "", 
+  previsao: "06/03 às 12:00", 
 };
 
 export default MANUTENCAO;
